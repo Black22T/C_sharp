e58c12e07c32c571df0f334391008dbc55cb09c7
Franco Gimenez <7francogimenez@gmail.com>
2024-06-09 21:04:05 -0300

    Agregar archivos de proyecto.

---
+++ simon_dice/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace simon_dice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rojoBoton = new System.Windows.Forms.Button();
+            this.amarilloBoton = new System.Windows.Forms.Button();
+            this.azulBoton = new System.Windows.Forms.Button();
+            this.verdeBoton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rojoBoton
+            // 
+            this.rojoBoton.BackColor = System.Drawing.Color.LightCoral;
+            this.rojoBoton.Location = new System.Drawing.Point(400, 50);
+            this.rojoBoton.Name = "rojoBoton";
+            this.rojoBoton.Size = new System.Drawing.Size(150, 150);
+            this.rojoBoton.TabIndex = 1;
+            this.rojoBoton.Text = "button2";
+            this.rojoBoton.UseVisualStyleBackColor = false;
+            this.rojoBoton.Click += new System.EventHandler(this.rojoBoton_Click);
+            // 
+            // amarilloBoton
+            // 
+            this.amarilloBoton.BackColor = System.Drawing.Color.Khaki;
+            this.amarilloBoton.Location = new System.Drawing.Point(200, 250);
+            this.amarilloBoton.Name = "amarilloBoton";
+            this.amarilloBoton.Size = new System.Drawing.Size(150, 150);
+            this.amarilloBoton.TabIndex = 2;
+            this.amarilloBoton.Text = "button3";
+            this.amarilloBoton.UseVisualStyleBackColor = false;
+            this.amarilloBoton.Click += new System.EventHandler(this.amarilloBoton_Click);
+            // 
+            // azulBoton
+            // 
+            this.azulBoton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.azulBoton.Location = new System.Drawing.Point(400, 250);
+            this.azulBoton.Name = "azulBoton";
+            this.azulBoton.Size = new System.Drawing.Size(150, 150);
+            this.azulBoton.TabIndex = 3;
+            this.azulBoton.Text = "button4";
+            this.azulBoton.UseVisualStyleBackColor = false;
+            this.azulBoton.Click += new System.EventHandler(this.azulBoton_Click);
+            // 
+            // verdeBoton
+            // 
+            this.verdeBoton.BackColor = System.Drawing.Color.PaleGreen;
+            this.verdeBoton.Location = new System.Drawing.Point(200, 50);
+            this.verdeBoton.Name = "verdeBoton";
+            this.verdeBoton.Size = new System.Drawing.Size(150, 150);
+            this.verdeBoton.TabIndex = 0;
+            this.verdeBoton.Text = "button1";
+            this.verdeBoton.UseVisualStyleBackColor = false;
+            this.verdeBoton.Click += new System.EventHandler(this.verdeBoton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(63, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "JUGAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Puntaje maximo: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.azulBoton);
+            this.Controls.Add(this.amarilloBoton);
+            this.Controls.Add(this.rojoBoton);
+            this.Controls.Add(this.verdeBoton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rojoBoton;
+        private System.Windows.Forms.Button amarilloBoton;
+        private System.Windows.Forms.Button azulBoton;
+        private System.Windows.Forms.Button verdeBoton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ simon_dice/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simon_dice
+{
+    public partial class Form1 : Form
+    {
+        Random randomBoton = new Random();
+        List<int> simondice = new List<int>();
+        List<int> jugador = new List<int>();
+        int indice, contador = 0, puntajemaximo=0;
+        float tiempoDeJuego = 5;
+        private DateTime tiempoInicio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (contador < simondice.Count)
+            {
+                iluminarboton(simondice[contador]);
+                contador++;
+                timer2.Start();
+            }
+            else
+            {
+                jugador.Clear();
+                contador = 0;
+                timer1.Stop();
+                tiempoInicio = DateTime.Now;
+                timer3.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            resetearColores();
+            timer2.Stop();
+        }
+
+        public void perdiste()
+        {
+            resetearColores();
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            
+            label1.Text = "Perdiste";
+            
+            jugador.Clear();
+            simondice.Clear();
+        }
+
+        public void ronda()
+        {
+            simondice.Add(randomBoton.Next(0, 4));
+            contador = 0;
+            indice = 0;
+            if (puntajemaximo < simondice.Count && jugador.Count > 0)
+            {
+                puntajemaximo = jugador.Count;
+                label2.Text = "Puntaje maximo: " + puntajemaximo;
+            }
+            jugador.Clear(); 
+            timer3.Stop();
+            label1.Text = "";
+            timer1.Start();
+            resetearColores();
+        }
+
+
+        public void iluminarboton(int botonIluminar)
+        {
+            switch (botonIluminar)
+            {
+                case 0:
+                    verdeBoton.BackColor = Color.Green;
+                    break;
+                case 1:
+                    rojoBoton.BackColor = Color.Red;
+                    break;
+                case 2:
+                    amarilloBoton.BackColor = Color.Yellow;
+                    break;
+                case 3:
+                    azulBoton.BackColor = Color.Blue;
+                    break;
+            }
+        }
+
+        /*public void compara()
+        {
+            if (simondice[indice] == jugador[indice])
+            {
+
+                indice++;
+                tiempoInicio = DateTime.Now;
+                timer3.Start();
+                if (indice == simondice.Count)
+                {
+                    ronda();
+                }
+            }
+            else
+            {
+                perdiste();
+            }
+        }*/
+
+        public void compara()
+        {
+            if (indice < simondice.Count && indice < jugador.Count)
+            {
+                if (simondice[indice] == jugador[indice])
+                {
+                    indice++;
+                    if (indice == simondice.Count)
+                    {
+                        ronda();
+                    }
+                    else
+                    {
+                        tiempoInicio = DateTime.Now;
+                        timer3.Start();
+                    }
+                }
+                else
+                {
+                    perdiste();
+                }
+            }
+            else
+            {
+                perdiste();
+            }
+        }
+
+        public void timerRun(int boton)
+        {
+            if (timer3.Enabled)
+            {
+                jugador.Add(boton);
+                compara();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            jugador.Clear();
+            simondice.Clear();
+            indice = 0;
+            ronda();
+        }
+
+        private void verdeBoton_Click(object sender, EventArgs e)
+        {
+            resetearColores();
+            iluminarboton(0);
+            timerRun(0);
+        }
+
+        private void rojoBoton_Click(object sender, EventArgs e)
+        {
+            resetearColores();
+            iluminarboton(1);
+            timerRun(1);
+        }
+
+        private void amarilloBoton_Click(object sender, EventArgs e)
+        {
+            resetearColores();
+            iluminarboton(2);
+            timerRun(2);
+        }
+
+        private void azulBoton_Click(object sender, EventArgs e)
+        {
+            resetearColores();
+            iluminarboton(3);
+            timerRun(3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            TimeSpan tiempoTranscurrido = DateTime.Now - tiempoInicio;
+            float tiempoRestante = tiempoDeJuego - (float)tiempoTranscurrido.TotalSeconds;
+
+            if (tiempoRestante <= 0)
+            {
+                timer3.Stop();
+                perdiste();
+            }
+            else
+            {
+                string tiempoRestanteCortado = tiempoRestante.ToString("F3");
+                label1.Text = tiempoRestanteCortado;
+            }
+        }
+
+        public void resetearColores()
+        {
+            verdeBoton.BackColor = Color.PaleGreen;
+            rojoBoton.BackColor = Color.LightCoral;
+            amarilloBoton.BackColor = Color.Khaki;
+            azulBoton.BackColor = Color.LightSteelBlue;
+        }
+    }
+}
